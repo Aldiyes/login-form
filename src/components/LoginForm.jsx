41da09b5d3b1be129e7a3a01c867593f9bda5a3b
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2022-09-01 17:26:39 +0700

    update query

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./login-form.css";
 
 const LoginForm = ({ Login, error }) => {
@@ -16,38 +19,52 @@ const LoginForm = ({ Login, error }) => {
     setShow((prevState) => !prevState);
   };
   return (
-    <form onSubmit={submitHandler}>
-      <div className="form-inner">
-        <header>
-          <h2>Sign In</h2>
-          <div className="icon">
-            <i className="fa-sharp fa-solid fa-robot"></i>
-          </div>
-        </header>
+    <section className="login-form">
+      <Container fluid="md" className="">
+        <Row className="justify-content-center">
+          <Col lg={6} xs={11}>
+            <form onSubmit={submitHandler}>
+              <div className="form-inner">
+                <header>
+                  <h2>Sign In</h2>
+                  <div className="icon">
+                    <i className="fa-sharp fa-solid fa-robot"></i>
+                  </div>
+                </header>
 
-        {error !== "" ? <div className="error">{error}</div> : ""}
-        <div className="form-group">
-          <i class="fa-sharp fa-solid fa-user"></i>
-          <input type="text" className="name" id="name" onChange={(e) => setDetails({ ...details, name: e.target.value })} value={details.name} required="required" />
-          <span>username</span>
-        </div>
-        <div className="form-group">
-          <i class="fa-sharp fa-solid fa-envelope"></i>
-          <input type="email" className="email" id="email" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} required="required" />
-          <span>email</span>
-        </div>
-        <div className="form-group">
-          <i class="fa-sharp fa-solid fa-key"></i>
-          <input type={show ? "text" : "password"} className="password" id="password" onChange={(e) => setDetails({ ...details, password: e.target.value })} value={details.password} required="required" />
-          <span>password</span>
-          <div className="show-password">
-            <i class={`fa-sharp fa-solid fa-eye ${show === true ? "d-none" : "d-flex"}`} onClick={handleShow}></i>
-            <i class={`fa-sharp fa-solid fa-eye-slash ${show === true ? "d-flex" : "d-none"}`} onClick={handleShow}></i>
-          </div>
-        </div>
-        <input type="submit" value="LOGIN" />
-      </div>
-    </form>
+                {error !== "" ? <div className="error">{error}</div> : ""}
+
+                <Row className="form-group">
+                  <Col xs={12} className="group-item">
+                    <i className="fa-sharp fa-solid fa-user"></i>
+                    <input type="text" className="name" id="name" onChange={(e) => setDetails({ ...details, name: e.target.value })} value={details.name} required="required" />
+                    <span>username</span>
+                  </Col>
+
+                  <Col xs={12} className="group-item">
+                    <i className="fa-sharp fa-solid fa-envelope"></i>
+                    <input type="mail" className="mail" id="mail" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} required="required" />
+                    <span>email</span>
+                  </Col>
+
+                  <Col xs={12} className="group-item">
+                    <i className="fa-sharp fa-solid fa-key"></i>
+                    <input type={show ? "text" : "password"} className="password" id="password" onChange={(e) => setDetails({ ...details, password: e.target.value })} value={details.password} required="required" />
+                    <span>password</span>
+                    <div className="show-password">
+                      <i className={`fa-sharp fa-solid fa-eye eye ${show === true ? "d-none" : "d-flex"}`} onClick={handleShow}></i>
+                      <i className={`fa-sharp fa-solid fa-eye-slash eye ${show === true ? "d-flex" : "d-none"}`} onClick={handleShow}></i>
+                    </div>
+                  </Col>
+                </Row>
+
+                <input type="submit" value="LOGIN" />
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
